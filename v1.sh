0626b049b7c2071073f6fbb40fe103d7bc0a68d5
lost1984 <yteinstein@163.com>
2020-02-25 11:16:46 +0800

    Update v1.sh

--- v1.sh
+++ v1.sh
@@ -1,4 +1,10 @@
-mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-cd /etc/ssh/
-wget https://github.com/lost1984/vps/blob/master/sshd_config
-service sshd restart
+git clone https://github.com/lost1984/vps
+cd vps
+#mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+#cd /etc/ssh/
+#wget https://github.com/lost1984/vps/blob/master/sshd_config
+#service sshd restart
+wget -N --no-check-certificate git.io/c.sh && chmod +x c.sh
+wget -N --no-check-certificate "https://github.000060000.xyz/tcp.sh" && chmod +x tcp.sh 
+wget -N --no-check-certificate git.io/superbench.sh && chmod +x superbench.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/aria2.sh
